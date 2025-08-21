8b69e742055c2c1f31455a935350e8e490ff050a
oldboni22 <oldboni22@gmail.com>
2025-08-22 00:49:21 +0300

    Added default value for the UserTopicRelation

--- UserService/UserService.DAL/Models/Entities/UserTopicStatus.cs
+++ UserService/UserService.DAL/Models/Entities/UserTopicStatus.cs
@@ -9,7 +9,7 @@ public class UserTopicStatus : EntityBase
     public Guid TopicId { get; init; }
 
     public User User { get; init; } = null!;
-    
-    public UserTopicRelation TopicRelation { get; set; }
-    
+
+    public UserTopicRelation TopicRelation { get; set; } = UserTopicRelation.Absent;
+
 }
--- UserService/UserService.DAL/Models/Enums/UserTopicRelation.cs
+++ UserService/UserService.DAL/Models/Enums/UserTopicRelation.cs
@@ -2,6 +2,7 @@ namespace UserService.DAL.Models.Enums;
 
 public enum UserTopicRelation 
 {
+    Absent = 0,
     Creator = 1,
     Moderator = 2,
     Subscribed = 3,
