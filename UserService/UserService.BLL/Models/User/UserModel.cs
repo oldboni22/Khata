9d11eba8314581cb7359c92728889b057a4bfab1
oldboni22 <oldboni22@gmail.com>
2025-08-22 20:21:15 +0300

    Removed redundant validation

--- UserService/UserService.BLL/Models/User/UserModel.cs
+++ UserService/UserService.BLL/Models/User/UserModel.cs
@@ -5,7 +5,6 @@ namespace UserService.BLL.Models.User;
 
 public class UserModel : ModelBase
 {
-    [MaxLength(15)]
     public required string Name { get; set; }
     
     public List<UserTopicRelation> TopicStatuses { get; set; } = [];
