5db0674f3f301617616a2b884a8c66d3a5c70e57
oldboni22 <oldboni22@gmail.com>
2025-08-28 17:54:16 +0300

    Removed redundant class

--- UserService/UserService.API/Utilities/MessageGenerators/Exceptions/InvalidEnumExceptionMessageGenerator.cs
+++
@@ -1,7 +0,0 @@
-namespace UserService.API.Utilities.MessageGenerators.Exceptions;
-
-public static class InvalidEnumExceptionMessageGenerator<T>
-{
-    public static string GenerateMessage() =>
-        $"Invalid enum string for {typeof(T).Name} was given.";
-}
