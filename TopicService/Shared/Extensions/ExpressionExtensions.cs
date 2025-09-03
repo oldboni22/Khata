1fe656c27b94fd88db4dee27a55ac6a6c61110cd
oldboni22 <oldboni22@gmail.com>
2025-09-03 20:26:16 +0300

    Fixed Expression And()

--- TopicService/Shared/Extensions/ExpressionExtensions.cs
+++ TopicService/Shared/Extensions/ExpressionExtensions.cs
@@ -4,13 +4,32 @@ namespace Shared.Extensions;
 
 public static class ExpressionExtensions
 {
+    // Честно, эта часть это уже полностью из ии
     public static Expression<Func<T, bool>> And<T>(
-        this Expression<Func<T, bool>> source, Expression<Func<T, bool>> addition)
+        this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
     {
-        var parameter = source.Parameters[0];
-        var invokedAddition = Expression.Invoke(addition, parameter);
-        
-        var newBody = Expression.AndAlso(source.Body, invokedAddition);
+        // Получаем параметр из первого выражения (например, "t =>")
+        var parameter = left.Parameters[0];
+
+        // Создаем визитор, который заменит параметр во втором выражении
+        // на параметр из первого
+        var visitor = new ReplaceParameterVisitor(right.Parameters[0], parameter);
+        var newRightBody = visitor.Visit(right.Body);
+
+        // Объединяем тела выражений через логическое "И"
+        var newBody = Expression.AndAlso(left.Body, newRightBody);
+
+        // Создаем новую лямбду с объединенным телом и оригинальным параметром
         return Expression.Lambda<Func<T, bool>>(newBody, parameter);
+    }
+
+    private class ReplaceParameterVisitor(
+        ParameterExpression oldParameter, ParameterExpression newParameter) : ExpressionVisitor
+    {
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            // Если узел - это тот параметр, который мы хотим заменить, заменяем его
+            return node == oldParameter ? newParameter : base.VisitParameter(node);
+        }
     }
 }
