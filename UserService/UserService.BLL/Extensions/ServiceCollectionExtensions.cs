d243970a33c8e39032720288ef6c1c1b7763f298
oldboni22 <oldboni22@gmail.com>
2025-10-06 11:40:33 +0300

    .

--- UserService/UserService.BLL/Extensions/ServiceCollectionExtensions.cs
+++ UserService/UserService.BLL/Extensions/ServiceCollectionExtensions.cs
@@ -67,6 +67,6 @@ public static class ServiceCollectionExtensions
 
     private static IServiceCollection AddServices(this IServiceCollection services)
     {
-        return services.AddScoped<IUserService, Services.UserRepository>();
+        return services.AddScoped<IUserService, Services.UserService>();
     }
 }
